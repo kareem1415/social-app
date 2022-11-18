4ab8b2cdfa2c83808c5e7b6bd143b399322ec808
kareem1415 <kareempython12@gmail.com>
2022-11-18 16:52:27 +0200

    change in port to delpoy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3003
+const port = process.env.PORT || 3000
 const middlware = require('./middleware')
 const path = require('path')
 const bodyParser = require('body-parser');
