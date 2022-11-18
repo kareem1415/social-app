de7aa89759e3bf8d9c5f969d7142354859838d5c
kareem1415 <kareempython12@gmail.com>
2022-11-18 21:15:47 +0200

    try to fix realtime

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,14 +1,8 @@
 let connected = false
 
-let socket = "ws://khamsa-bako.herokuapp.com:3000";
-let ws = new WebSocket(socket);
-let el = document.getElementById('server-time');
-ws.onmessage = function (event) {
-  el.innerHTML = 'Server time: ' + event.data;
-};
+let socket = io("https://khamsa-bako.herokuapp.com/")
 
 socket.emit("setup", userLoggedIn)
 
 socket.on("connected", () => connected = true)
 socket.on("message received", (newMessage) => messageReceived(newMessage))
-
